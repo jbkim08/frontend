97bda17fcbb1b261eb72ae9f3d80b8a5028da7a1
JB <drv98@naver.com>
2023-12-26 15:14:17 +0900

    유저들정보 표시

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
 
   const getUsers = async () => {
     const result = await axios.get('http://localhost:8080/users');
-    console.log(result);
+    setUsers(result.data);
   };
   return (
     <div className="container">
@@ -19,29 +19,20 @@ function Home() {
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">이름</th>
+            <th scope="col">유저네임</th>
+            <th scope="col">이메일</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
