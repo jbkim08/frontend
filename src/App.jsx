d44304192e2833e90a502660ce4d2286dea46906
JB <drv98@naver.com>
2023-12-26 14:39:11 +0900

    테으블을 넣은 홈을 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Nabvar from './layout/Nabvar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Nabvar />
+      <Home />
     </div>
   );
 }
