4c42c148ffad57ed8b903b09025377d390900b29
JB <drv98@naver.com>
2023-12-26 15:26:58 +0900

    라우터 적용 홈과 유저추가 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import Nabvar from './layout/Nabvar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddUser from './users/AddUser';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Nabvar />
-      <Home />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/adduser" element={<AddUser />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
