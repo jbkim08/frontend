69d5532f39b49ba3c02bb000cd66ab8125a8f551
JB <drv98@naver.com>
2023-12-26 15:29:50 +0900

    링크로 페이지 이동

--- src/layout/Nabvar.jsx
+++ src/layout/Nabvar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nabvar() {
   return (
     <nav className="navbar navbar-expand-md bg-primary navbar-dark">
       <div className="container-fluid">
-        <a className="navbar-brand text-warning" href="#">
+        <Link to="/" className="navbar-brand text-warning">
           리액트 & 스프링부트
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,7 +22,9 @@ function Nabvar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <button className="btn btn-outline-light">유저 추가</button>
+              <Link to="/addUser" className="btn btn-outline-light">
+                유저 추가
+              </Link>
             </li>
           </ul>
         </div>
