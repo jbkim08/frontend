d7a248ecb5e90c8bd798c66702d39e956fd36985
KJB <drv98@naver.com>
2025-12-15 11:09:49 +0900

    패키지폴더와 jsx 파일 만들기

---
+++ src/users/AddUser.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AddUser() {
+  return <div>애드유저</div>;
+}
---
+++ src/users/EditUser.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditUser() {
+  return <div>에딧유저</div>;
+}
