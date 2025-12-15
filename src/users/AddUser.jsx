9f08b94a21783feaba097493199a028ea11e0dfe
KJB <drv98@naver.com>
2025-12-15 14:28:35 +0900

    서브밋 이벤트

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -16,61 +16,70 @@ export default function AddUser() {
       [e.target.name]: e.target.value,
     });
   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("서브밋 이벤트 정지");
+  };
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">가입 하기</h2>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              이름
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={onInputChange}
-              className="form-control"
-              placeholder="이름 입력"
-              name="name"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              유저네임
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={onInputChange}
-              className="form-control"
-              placeholder="유저네임 입력"
-              name="username"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              이메일
-            </label>
-            <input
-              type="text"
-              id="email"
-              value={email}
-              onChange={onInputChange}
-              className="form-control"
-              placeholder="이메일 입력"
-              name="email"
-            />
-          </div>
-          <div className="mb-3 text-center">
-            <button type="submit" className="btn btn-outline-primary px-3 mx-2">
-              가입
-            </button>
-            <Link to="/" className="btn btn-outline-danger px-3 mx-2">
-              취소
-            </Link>
-          </div>
+          <form onSubmit={onSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                이름
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={onInputChange}
+                className="form-control"
+                placeholder="이름 입력"
+                name="name"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                유저네임
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={onInputChange}
+                className="form-control"
+                placeholder="유저네임 입력"
+                name="username"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                이메일
+              </label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={onInputChange}
+                className="form-control"
+                placeholder="이메일 입력"
+                name="email"
+              />
+            </div>
+            <div className="mb-3 text-center">
+              <button
+                type="submit"
+                className="btn btn-outline-primary px-3 mx-2"
+              >
+                가입
+              </button>
+              <Link to="/" className="btn btn-outline-danger px-3 mx-2">
+                취소
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
     </div>
