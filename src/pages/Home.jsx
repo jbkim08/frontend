23b864c759dae857cd92b4c426f47883b2c6226f
KJB <drv98@naver.com>
2025-12-15 12:06:51 +0900

    화면에 유저 출력

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,37 @@
 import axios from "axios"; //액시오스 객체
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [users, setUsers] = useState([]); //유저객체 배열
   //백엔드에서 유저들을 가져오는 함수
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:8080/users");
-    console.log(result);
+    setUsers(result.data);
   };
-  loadUsers();
+  //처음 한번 실행됨
+  useEffect(() => {
+    loadUsers();
+  }, []);
   return (
     <div className="container">
       <table className="table table-hover border table-border shadow my-4">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">이름</th>
+            <th scope="col">유저네임</th>
+            <th scope="col">이메일</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
