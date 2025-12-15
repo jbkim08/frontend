e73d047474a7a756790fd480ebfa6aef36367e90
KJB <drv98@naver.com>
2025-12-15 14:02:55 +0900

    유저추가 화면

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function AddUser() {
-  return <div>유저추가</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">가입 하기</h2>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              이름
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="form-control"
+              placeholder="이름 입력"
+              name="name"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
