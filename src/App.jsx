feb8186e5ee56b70e7092c141ebcf1741fbc5fb3
KJB <drv98@naver.com>
2025-12-16 10:03:05 +0900

    뷰 유저 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddUser from "./users/AddUser";
 import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/adduser" element={<AddUser />} />
           <Route path="/edituser/:id" element={<EditUser />} />
+          <Route path="/viewuser/:id" element={<ViewUser />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/users/ViewUser.jsx
+++ src/users/ViewUser.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ViewUser() {
-  return <div>뷰유저</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">유저 정보</h2>
+
+          <div className="card">
+            <div className="card-header">
+              유저ID :
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <b>이름 : </b>
+                </li>
+                <li className="list-group-item">
+                  <b>유저네임 : </b>
+                </li>
+                <li className="list-group-item">
+                  <b>이메일 : </b>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Link className="btn btn-primary my-2" to={"/"}>
+            돌아기기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
